3b83c9e8418cbcae58904ebeff9b5d11bc868c52
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2026-04-24 13:33:21 +0700

    fix: hashcode function

--- src/main/java/com/gojek/mc2pinot/core/partition/HashCodePartitionFunction.java
+++ src/main/java/com/gojek/mc2pinot/core/partition/HashCodePartitionFunction.java
@@ -4,7 +4,8 @@ public class HashCodePartitionFunction implements PartitionFunction {
 
     @Override
     public int partition(String value, int numPartitions) {
-        return (value.hashCode() & Integer.MAX_VALUE) % numPartitions;
+        int h = value.hashCode();
+        return (h == Integer.MIN_VALUE ? 0 : Math.abs(h)) % numPartitions;
     }
 }
 
