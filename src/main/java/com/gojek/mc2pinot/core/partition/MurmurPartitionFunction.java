07b2f89045681091958dacff39c63b7a6423d004
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2026-04-24 13:40:12 +0700

    fix: use partition function from pinot sdk

--- src/main/java/com/gojek/mc2pinot/core/partition/MurmurPartitionFunction.java
+++ src/main/java/com/gojek/mc2pinot/core/partition/MurmurPartitionFunction.java
@@ -1,15 +1,11 @@
 package com.gojek.mc2pinot.core.partition;
 
-import com.google.common.hash.Hashing;
-
-import java.nio.charset.StandardCharsets;
-
 public class MurmurPartitionFunction implements PartitionFunction {
 
     @Override
     public int partition(String value, int numPartitions) {
-        int hash = Hashing.murmur3_32_fixed(0).hashString(value, StandardCharsets.UTF_8).asInt();
-        return (hash & Integer.MAX_VALUE) % numPartitions;
+        return new org.apache.pinot.segment.spi.partition.MurmurPartitionFunction(numPartitions)
+                .getPartition(value);
     }
 }
 
